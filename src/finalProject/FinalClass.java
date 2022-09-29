9196fec66cddc3387abf797fb95cddf244377417
MoAyoub <mohdayoub50@gmail.com>
2022-09-28 19:05:40 -0500

    First commit

---
+++ src/finalProject/FinalClass.java
@@ -0,0 +1,10 @@
+package finalProject;
+
+public class FinalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is the code for the Ferrari");
+	}
+
+}
