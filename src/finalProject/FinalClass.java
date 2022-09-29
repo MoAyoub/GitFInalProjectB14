f282344c48eb37986c4ece65b635cf7e38a87fb3
MoAyoub <mohdayoub50@gmail.com>
2022-09-28 19:50:54 -0500

    Second commit

--- src/finalProject/FinalClass.java
+++ src/finalProject/FinalClass.java
@@ -5,6 +5,15 @@ public class FinalClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is the code for the Ferrari");
+
+
+int z=2,sum=0;
+while(z<9) {
+	z++;
+	sum=sum+z;
+	
+}
+System.out.println(sum);
 	}
 
 }
